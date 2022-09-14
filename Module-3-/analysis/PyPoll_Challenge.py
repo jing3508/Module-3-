2bc32ecf17f73bb6179877d9b5388415d59bea44
Jingwen Zhou <112013227+jing3508@users.noreply.github.com>
2022-09-14 17:01:26 -0400

    Add files via upload

--- Module-3-/analysis/PyPoll_Challenge.py
+++ Module-3-/analysis/PyPoll_Challenge.py
@@ -6,9 +6,9 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("resources", "election_results.csv")
+file_to_load = os.path.join("..","resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("..","analysis", "election_analysis.txt")
 
 # Initialize a total vote counter.
 
